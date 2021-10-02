e7ab5d84b8a49f299c11206fcb44260ded213fb1
kaynquang <91773837+kaynquang@users.noreply.github.com>
2021-10-02 12:55:27 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Quỳnh Mai bae ơi !',
+    introDesc: `tui có chuyện này muốn hỏi bae nè `,
+    btnIntro: 'Em đây',
+    title: 'Có phải em đã yêu anh từ cái nhìn đầu không 😙',
+    desc: 'Em có say anh không ',
+    btnYes: 'Em rất thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vì anh mập mạp dễ thương đó nòa  <33333333',
+    mess: 'tui biết mà 🥰. iuuu bae của Hoàng nhiều nhiều nòa  😘😘',
+    messDesc: 'Tối nay 7h mình cùng call ôn lý nhá .',
+    btnAccept: 'Okie lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100026239419316' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
